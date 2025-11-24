93bbbffb17e6189bd3972af0439c7d0d6371c18f
Mahabub Ahmed <121817382+Mahabub175@users.noreply.github.com>
2025-11-24 18:47:54 +0600

    Update blog.model.ts

--- src/app/modules/blog/blog.model.ts
+++ src/app/modules/blog/blog.model.ts
@@ -1,6 +1,9 @@
 import { Schema, model } from "mongoose";
 import { IBlog } from "./blog.interface";
 import { generateSlug } from "../../utils/generateSlug";
+import config from "../../config/config";
+
+const BASE_URL = config.base_url || "https://example.com";
 
 const blogSchema = new Schema<IBlog>(
   {
@@ -11,6 +14,7 @@ const blogSchema = new Schema<IBlog>(
     publishedAt: { type: String, required: true, trim: true },
     attachment: { type: String },
     images: { type: [String], default: undefined },
+    isDeleted: { type: Boolean, default: false },
     status: {
       type: Boolean,
       default: true,
@@ -18,6 +22,21 @@ const blogSchema = new Schema<IBlog>(
   },
   { timestamps: true }
 );
+
+blogSchema.options.toJSON = blogSchema.options.toJSON || {};
+blogSchema.options.toJSON.transform = function (doc, ret: any) {
+  if (!ret) return ret;
+
+  if (ret.attachment && typeof ret.attachment === "string") {
+    ret.attachment = `${BASE_URL}/${ret.attachment}`;
+  }
+
+  if (Array.isArray(ret.images)) {
+    ret.images = ret.images.map((img: string) => `${BASE_URL}/${img}`);
+  }
+
+  return ret;
+};
 
 blogSchema.pre("validate", async function (next) {
   if (!this.slug && this.name) {
