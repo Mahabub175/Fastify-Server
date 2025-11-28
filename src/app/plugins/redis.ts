623fe953eb66b301fa564c12510dcf74b32a6a87
Mahabub Ahmed <121817382+Mahabub175@users.noreply.github.com>
2025-11-28 18:19:31 +0600

    Update redis.ts

--- src/app/plugins/redis.ts
+++ src/app/plugins/redis.ts
@@ -4,9 +4,9 @@ import config from "../config/config";
 
 export const redisPlugin = fastifyPlugin(async (fastify, opts) => {
   const redis = new Redis({
-    host: "127.0.0.1",
-    port: 6379,
-    password: undefined,
+    host: config.redis_host || "127.0.0.1",
+    port: Number(config.redis_port) || 6379,
+    password: config.redis_password || undefined,
     retryStrategy(times) {
       return Math.min(times * 50, 2000);
     },
@@ -41,3 +41,4 @@ declare module "fastify" {
     redis: Redis;
   }
 }
+
